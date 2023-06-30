49f94b70bc9108199bb18c05e5730bd2bc59315d
Anant <anantpatel07012003@gmail.com>
2023-06-30 12:55:52 +0530

    img

--- src/Components/Pages/AboutUs.js
+++ src/Components/Pages/AboutUs.js
@@ -1,5 +1,15 @@
 import React from "react";
 import classes from "./AboutUs.module.css";
+import img from "../../Assets/top-distant-view-table-with-vegetables-such-as-tomatoes-cucumbers-along-with-lemons-crisps-greens-white-salad-vegetable-food.jpg";
 export default function AboutUs() {
-  return <div className={classes.aboutus}>somla;sd</div>;
+  return (
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <img className={classes.aboutUsImg} src={img} alt="grocerry" />
+        </div>
+        <div class="col-lg-6"></div>
+      </div>
+    </div>
+  );
 }
